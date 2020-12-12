e05d0996b024260b6fba8c9b2a5782cd838c9d74
ravikumarkubusada <48872854+ravikumarkubusada@users.noreply.github.com>
2020-12-12 12:01:09 +0530

    initial boiler plate code

---
+++ blood_bank_api/src/main/java/com/demo/blood_bank_api/BloodBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.blood_bank_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankApiApplication.class, args);
+	}
+
+}

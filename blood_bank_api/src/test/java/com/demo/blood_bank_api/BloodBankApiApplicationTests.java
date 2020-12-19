dc661c5b258da1cae0b9102e1baf48b82a2fc680
ravikumarkubusada <48872854+ravikumarkubusada@users.noreply.github.com>
2020-12-19 17:56:04 +0530

    commenting all the test class

--- blood_bank_api/src/test/java/com/demo/blood_bank_api/BloodBankApiApplicationTests.java
+++ blood_bank_api/src/test/java/com/demo/blood_bank_api/BloodBankApiApplicationTests.java
@@ -1,13 +1,13 @@
 package com.demo.blood_bank_api;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BloodBankApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
